852b72bb17bfcabee1745408d42b211f6013e95c
martin.borba <m.borba98@gmail.com>
2020-10-07 18:48:03 -0300

    andapartedeanounce

--- LANSharing.py
+++ LANSharing.py
@@ -1,12 +1,21 @@
 import socket
 import time
-import _thread
-import hashlib
-import random
-import os.path
-from threading import Thread, Lock
+import _thread 
+import hashlib  #md5
+import random   #espera aleatorea de tiempos
+import os.path  #chequear que existe un archivo
+import re #splitear los mensajes del protocolo separados por tabs y enters
+from threading import Lock  #mutuoexcluir el acceso a estructuras de datos
 
-#MODIFICO MARTIN
+#Posiciones en lo recibido por anuncios
+fileName=0
+fileSize=1
+fileMd5=2
+
+#Posiciones en los diccionarios
+Seeders=2
+ttls=3
+
 
 dirFran= "25.92.62.202"
 dirMartin= "25.91.200.244"
@@ -21,17 +30,48 @@ def md5(fname):
 
 def enviarAnuncios(scktAnuncio):
     while True:
-        time.sleep(30)
+        time.sleep(5)
         time.sleep(random.uniform(0.5,1))
-        anuncio = "ANNOUNCE\n"
-        for key in archivosLocales:
-            anuncio += archivosLocales[key][0] + "\t" + str(archivosLocales[key][1]) + "\t" + str(key) +"\n"
-        scktAnuncio.sendto((anuncio).encode(),(dirBroadcast,2020))
+        print(archivosDeRed)
+        if( bool(archivosLocales)): #archivosLocales no vacíos
+            anuncio = "ANNOUNCE\n"
+            mutexLocales.acquire() #mutuoexcluimos archivosLocales
+            for key in archivosLocales:
+                anuncio += archivosLocales[key][0] + "\t" + str(archivosLocales[key][1]) + "\t" + str(key) +"\n"
+            mutexLocales.release() #liberamos archivosLocales
+            scktAnuncio.sendto((anuncio).encode(),(dirBroadcast,2020))
+            
 
 def recibirAnuncios(scktEscucha):
     while True:
-        mensaje = scktEscucha.recv(1024) #escucha con un buffer de 1024bytes(1024 chars) en el 2020
-        print(mensaje.decode())
+        mensaje,addr = scktEscucha.recvfrom(1024) #escucha con un buffer de 1024bytes(1024 chars) en el 2020
+        #if(addr==socket.gethostbyname("localhost")): #no queremos escuchar nuestro propio broadcast
+        lineas=re.split(r'\n+', mensaje.decode())
+
+        if(lineas[0]=="ANNOUNCE"):
+            iterLinea = iter(lineas)
+            next(iterLinea)
+            for linea in iterLinea:
+                datos=re.split(r'\t+', linea)
+                #print("causa de error:"+str(datos))
+                if( datos[0]!=''): # para evitar la última linea que viene vacía
+                    mutexRed.acquire() 
+                    if( datos[fileMd5] in archivosDeRed): #sólo debemos agregar el nuevo seeder
+                        if(not (addr[0] in archivosDeRed[datos[fileMd5]][Seeders]) ): # efectivamente es nuevo
+                            archivosDeRed[datos[fileMd5]][Seeders].append(addr[0])
+                            archivosDeRed[datos[fileMd5]][ttls].append(3) 
+                        else: #seeder conocido, corresponde actualizar su ttl
+                            indexSeeder=archivosDeRed[datos[fileMd5]][Seeders].index(addr[0])
+                            archivosDeRed[datos[fileMd5]][ttls][indexSeeder]=3
+                    else:#agregar nuevo archivo
+                        archivosDeRed[datos[fileMd5]]=[datos[fileName],datos[fileSize],[addr[0]],[3]]
+                    mutexRed.release()
+
+
+
+        if(lineas[0]=="REQUEST"):
+            print("retornar request")
+
 
 
 def verCompartidos():
@@ -58,9 +98,11 @@ if __name__ == '__main__':
     scktEscucha.bind(("", 2020))
 
     #archivos de Red
-    archivosDeRed = {}
+    archivosDeRed = {}  #md5 : nombre , tamaño, Seeders[], ttl[]
+    mutexRed = Lock()
     #archivos locales
-    archivosLocales = {}
+    archivosLocales = {} #md5 : nombre , tamaño 
+    mutexLocales = Lock()
 
     try:
         _thread.start_new_thread(enviarAnuncios,(scktAnuncio, ))
