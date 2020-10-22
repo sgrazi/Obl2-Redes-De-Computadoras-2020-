af88182a6d1939c1d58420ba9607db2cea0eff20
martin.borba <m.borba98@gmail.com>
2020-10-22 01:47:33 -0300

    deje andando una demo de telnet en prueba.py

--- prueba.py
+++ prueba.py
@@ -1,4 +1,22 @@
 import socket
+from threading import Lock 
 
-open(pathfile, "rb")
-open(pathfile, "rb") as f:
+mutexTelnet=Lock()
+
+while 1:
+    masterTelnet = socket.socket()
+    masterTelnet.bind(("", 2025))
+    masterTelnet.listen()
+    sktTelnet,addr =masterTelnet.accept()
+    comando=b''
+    char=b''
+    while 1:
+        char =sktTelnet.recv(10)
+        comando+=char
+        if char.decode("unicode_escape")=="\r\n":
+            retorno="Recibido: ".encode("unicode_escape")+comando
+            sktTelnet.sendall(retorno)
+            comando=b''
+            char=b''
+
+
